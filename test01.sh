901d72ada74f8f86d5b2e6ca5513fc2d6dfa82db
Daniel Dimitrov <596076@gmail.com>
2019-07-27 13:53:40 +0300

    modify test01

--- test01.sh
+++ test01.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#trying to execute remote script 'bash <(curl -s https://raw.githubusercontent.com/verb5/shell/master/test01.sh)'
 echo "Hello, world!"
 read -p "What is your name? " name
 echo "Hello, ${name}!"
